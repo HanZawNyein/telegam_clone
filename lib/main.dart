b8b5eb7d99d867c45bea846c1ebed90720149b3f
HanZawNyein <HanZawNyein>
2023-04-10 18:28:40 +0630

    add telegram clone

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_clone/user_card.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.teal,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Map> users = [
+    {
+      'url': 'assets/images/man1.jpg',
+      'name': "Han Zaw Nyein",
+      'time': "11:11 PM",
+      'lastText': "How are you ?",
+      'unreadCount': "3"
+    },
+    {
+      'url': 'assets/images/man2.webp',
+      'name': "Han Zaw",
+      'time': "11:11 PM",
+      'lastText': "Hi",
+      'unreadCount': "10"
+    },
+    {
+      'url': 'assets/images/man3.webp',
+      'name': "Han Zaw",
+      'time': "11:11 PM",
+      'lastText': "Hi",
+      'unreadCount': "50"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: const Color(0xff55979F),
+        leading: Icon(Icons.menu),
+        title: Text(
+          "Telegram",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Column(
+          children: users
+              .map(
+                (user) => UserCard(
+                  url: user["url"],
+                  name: user["name"],
+                  time: user["time"],
+                  lastText: user["lastText"],
+                  unreadCount: user["unreadCount"],
+                ),
+              )
+              .toList()),
+    );
+  }
+}
---
+++ lib/user_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class UserCard extends StatelessWidget {
+  final String url;
+  final String name;
+  final String time;
+  final String lastText;
+  final String unreadCount;
+
+  const UserCard(
+      {super.key,
+      required this.url,
+      required this.name,
+      required this.time,
+      required this.lastText,
+      required this.unreadCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.only(top: 5, left: 10),
+      leading: CircleAvatar(
+        radius: 30.0,
+        backgroundImage: ExactAssetImage(url),
+        // child: ,
+      ),
+      title: Padding(
+        padding: const EdgeInsets.only(right: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              name,
+              style:
+                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 19.0),
+            ),
+            Text(
+              time,
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(right: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              lastText,
+              style: const TextStyle(fontWeight: FontWeight.w500),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3),
+                  borderRadius: BorderRadius.circular(70),
+                  color: Colors.teal),
+              child: Text(
+                unreadCount,
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
